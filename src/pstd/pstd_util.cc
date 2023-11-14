8fa566be0da343244051c572e8b913ba867f2960
Qx <lqxlucky@qq.com>
2023-11-14 15:55:12 +0800

    Unify the naming of pstd functions and add random functions (#35)
    * Unify the naming of pstd functions and add random functions
    
    * Rename the function
    
    * fix use function name error

---
+++ src/pstd/pstd_util.cc
@@ -0,0 +1,31 @@
+// Copyright (c) 2015-present, Qihoo, Inc.  All rights reserved.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include <algorithm>
+
+#include "pstd_util.h"
+
+namespace pstd {
+
+std::mt19937 gen;
+
+void InitRandom() {
+  std::random_device rd;
+  gen.seed(rd());
+}
+
+int RandomInt(int max) { return RandomInt(0, max + 1); }
+
+int RandomInt(int min, int max) {
+  std::uniform_int_distribution<> dist(min, max);
+  return dist(gen);
+}
+
+double RandomDouble() {
+  static std::uniform_real_distribution<double> dis(0.0, 1.0);
+  return dis(gen);
+}
+
+}  // namespace pstd
