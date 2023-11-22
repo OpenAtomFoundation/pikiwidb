a32aee0a89fce073377dc61fb0395845a1bdcbcc
lqx <lqxlucky@qq.com>
2023-11-22 22:42:46 +0800

    Resolve rocksdb and gtest dependency conflicts

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -10,5 +10,22 @@ FetchContent_Declare(
         GIT_TAG v8.6.7
 )
 
+FetchContent_GetProperties(rocksdb)
+
+if(NOT rocksdbPOPULATED)
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "")
+    set(CMAKE_WITH_TESTS OFF CACHE BOOL "")
+    set(CMAKE_WITH_BENCHMARK OFF CACHE BOOL "")
+    set(CMAKE_WITH_BENCHMARK_TOOLS OFF CACHE BOOL "")
+    set(CMAKE_WITH_TOOLS OFF CACHE BOOL "")
+    set(CMAKE_WITH_CORE_TOOLS OFF CACHE BOOL "")
+    set(CMAKE_WITH_TRACE_TOOLS OFF CACHE BOOL "")
+    set(CMAKE_WITH_EXAMPLES OFF CACHE BOOL "")
+    set(CMAKE_ROCKSDB_BUILD_SHARED OFF CACHE BOOL "")
+    set(CMAKE_WITH_GFLAGS ON CACHE BOOL "")
+    set(CMAKE_WITH_LIBURING OFF CACHE BOOL "")
+    FetchContent_Populate(rocksdb)
+endif()
+
 FetchContent_MakeAvailable(rocksdb)
 include_directories(${rocksdb_SOURCE_DIR}/include)
