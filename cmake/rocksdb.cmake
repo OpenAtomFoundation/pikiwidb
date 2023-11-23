c2e1344935d0f50bc818bfd58d149d40d8789fe5
lqx <lqxlucky@qq.com>
2023-11-23 22:05:10 +0800

    fix rocksdb and gtest dependency conflicts,Streamlined rocksdb compilation

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -10,22 +10,16 @@ FetchContent_Declare(
         GIT_TAG v8.6.7
 )
 
-FetchContent_GetProperties(rocksdb)
-
-if(NOT rocksdbPOPULATED)
-    set(CMAKE_BUILD_TYPE Release CACHE STRING "")
-    set(CMAKE_WITH_TESTS OFF CACHE BOOL "")
-    set(CMAKE_WITH_BENCHMARK OFF CACHE BOOL "")
-    set(CMAKE_WITH_BENCHMARK_TOOLS OFF CACHE BOOL "")
-    set(CMAKE_WITH_TOOLS OFF CACHE BOOL "")
-    set(CMAKE_WITH_CORE_TOOLS OFF CACHE BOOL "")
-    set(CMAKE_WITH_TRACE_TOOLS OFF CACHE BOOL "")
-    set(CMAKE_WITH_EXAMPLES OFF CACHE BOOL "")
-    set(CMAKE_ROCKSDB_BUILD_SHARED OFF CACHE BOOL "")
-    set(CMAKE_WITH_GFLAGS ON CACHE BOOL "")
-    set(CMAKE_WITH_LIBURING OFF CACHE BOOL "")
-    FetchContent_Populate(rocksdb)
-endif()
-
-FetchContent_MakeAvailable(rocksdb)
-include_directories(${rocksdb_SOURCE_DIR}/include)
+FetchContent_MakeAvailableWithArgs(rocksdb
+        BUILD_TYPE=OFF
+        WITH_TESTS=OFF
+        WITH_BENCHMARK=OFF
+        WITH_BENCHMARK_TOOLS=OFF
+        WITH_TOOLS=OFF
+        WITH_CORE_TOOLS=OFF
+        WITH_TRACE_TOOLS=OFF
+        WITH_EXAMPLES=OFF
+        ROCKSDB_BUILD_SHARED=OFF
+        WITH_GFLAGS=OFF
+        WITH_LIBURING=OFF
+)
