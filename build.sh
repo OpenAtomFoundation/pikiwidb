cd138d969425a6ce15a128c4d5ffe0c090e01537
lqx <lqxlucky@qq.com>
2023-11-27 20:10:56 +0800

    add build.sh read prams,add ci on ubuntu

--- build.sh
+++ build.sh
@@ -12,6 +12,49 @@ BUILD_TIME=${BUILD_TIME: 0: 10}
 COMMIT_ID=$(git rev-parse HEAD)
 SHORT_COMMIT_ID=${COMMIT_ID: 0: 8}
 
+BUILD_TYPE=release
+VERBOSE=0
+CMAKE_FLAGS=""
+MAKE_FLAGS=""
+PREFIX="build"
+
+ARGS=`getopt -a -o h -l help,debug,verbose,prefix: -- "$@"`
+function show_help() {
+  echo "
+  -h --help   show help
+  --debug     compile with debug
+  --verbose   compile with verbose
+  --prefix    compile output path
+  "
+  exit 0
+}
+
+eval set -- "${ARGS}"
+while true
+do
+      case "$1" in
+      -h|--help)
+              show_help
+              ;;
+      --debug)
+              BUILD_TYPE=debug
+              ;;
+      --verbose)
+              CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+              MAKE_FLAGS="${MAKE_FLAGS} VERBOSE=1"
+              ;;
+      --prefix)
+              PREFIX=$2
+              shift
+              ;;
+      --)
+              shift
+              break
+              ;;
+      esac
+shift
+done
+
 if [ ! -f "/proc/cpuinfo" ];then
   CPU_CORE=$(sysctl -n hw.ncpu)
 else
@@ -31,8 +74,12 @@ fi
 echo "BUILD_TIME:" $BUILD_TIME
 echo "COMMIT_ID:" $SHORT_COMMIT_ID
 
-cmake -DCMAKE_BUILD_TYPE=release -DBUILD_TIME=$BUILD_TIME -DGIT_COMMIT_ID=$SHORT_COMMIT_ID -S . -B build
-cmake --build build -- -j ${CPU_CORE}
+echo "BUILD_TYPE:" $BUILD_TYPE
+echo "CMAKE_FLAGS:" $CMAKE_FLAGS
+echo "MAKE_FLAGS:" $MAKE_FLAGS
+
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_TIME=$BUILD_TIME -DGIT_COMMIT_ID=$SHORT_COMMIT_ID ${CMAKE_FLAGS} -S . -B ${PREFIX}
+cmake --build ${PREFIX} -- ${MAKE_FLAGS} -j ${CPU_CORE}
 
 if [ $? -eq 0 ]; then
     echo -e "pika compile complete, output file ${C_GREEN} ${BUILD_DIR}/pika ${C_END}"
