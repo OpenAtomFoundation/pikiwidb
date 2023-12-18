a947feee3f0cefa518f55f9ad9dccb558b4440c4
Jett <jetttcc@gmail.com>
2023-12-19 00:39:53 +0800

    feat: implement the getrange command

--- src/cmd_table_manager.cc
+++ src/cmd_table_manager.cc
@@ -58,6 +58,7 @@ void CmdTableManager::InitCmdTable() {
   ADD_COMMAND(BitOp, -4);
   ADD_COMMAND(BitCount, -2);
   ADD_COMMAND(GetBit, 3);
+  ADD_COMMAND(GetRange, 4);
 
   // hash
   ADD_COMMAND(HSet, -4);
