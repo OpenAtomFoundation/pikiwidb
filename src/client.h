13a6262c9a3c0b9e08e0294c8344e2d91cb25802
hero-heng <2374114527@qq.com>
2024-01-06 13:36:29 +0800

    feat:update code style

--- src/client.h
+++ src/client.h
@@ -1,9 +1,9 @@
 /*
-* Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
-* This source code is licensed under the BSD-style license found in the
-* LICENSE file in the root directory of this source tree. An additional grant
-* of patent rights can be found in the PATENTS file in the same directory.
-*/
+ * Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #pragma once
 
@@ -21,227 +21,227 @@
 namespace pikiwidb {
 
 class CmdRes {
-public:
- enum CmdRet {
-   kNone = 0,
-   kOK,
-   kPong,
-   kSyntaxErr,
-   kInvalidInt,
-   kInvalidBitInt,
-   kInvalidBitOffsetInt,
-   kInvalidBitPosArgument,
-   kWrongBitOpNotNum,
-   kInvalidFloat,
-   kOverFlow,
-   kNotFound,
-   kOutOfRange,
-   kInvalidPwd,
-   kNoneBgsave,
-   kPurgeExist,
-   kInvalidParameter,
-   kWrongNum,
-   kInvalidIndex,
-   kInvalidDbType,
-   kInvalidDB,
-   kInconsistentHashTag,
-   kErrOther,
-   KIncrByOverFlow,
- };
+ public:
+  enum CmdRet {
+    kNone = 0,
+    kOK,
+    kPong,
+    kSyntaxErr,
+    kInvalidInt,
+    kInvalidBitInt,
+    kInvalidBitOffsetInt,
+    kInvalidBitPosArgument,
+    kWrongBitOpNotNum,
+    kInvalidFloat,
+    kOverFlow,
+    kNotFound,
+    kOutOfRange,
+    kInvalidPwd,
+    kNoneBgsave,
+    kPurgeExist,
+    kInvalidParameter,
+    kWrongNum,
+    kInvalidIndex,
+    kInvalidDbType,
+    kInvalidDB,
+    kInconsistentHashTag,
+    kErrOther,
+    KIncrByOverFlow,
+  };
 
- CmdRes() = default;
- virtual ~CmdRes();
+  CmdRes() = default;
+  virtual ~CmdRes();
 
- bool None() const { return ret_ == kNone && message_.empty(); }
+  bool None() const { return ret_ == kNone && message_.empty(); }
 
- bool Ok() const { return ret_ == kOK || ret_ == kNone; }
+  bool Ok() const { return ret_ == kOK || ret_ == kNone; }
 
- void Clear() {
-   message_.clear();
-   ret_ = kNone;
- }
+  void Clear() {
+    message_.clear();
+    ret_ = kNone;
+  }
 
- inline const std::string& Message() const { return message_; };
+  inline const std::string& Message() const { return message_; };
 
- // Inline functions for Create Redis protocol
- inline void AppendStringLen(int64_t ori) { RedisAppendLen(message_, ori, "$"); }
- inline void AppendStringLenUint64(uint64_t ori) { RedisAppendLenUint64(message_, ori, "$"); }
- inline void AppendArrayLen(int64_t ori) { RedisAppendLen(message_, ori, "*"); }
- inline void AppendArrayLenUint64(uint64_t ori) { RedisAppendLenUint64(message_, ori, "*"); }
- inline void AppendInteger(int64_t ori) { RedisAppendLen(message_, ori, ":"); }
- inline void AppendContent(const std::string& value) { RedisAppendContent(message_, value); }
- inline void AppendStringRaw(const std::string& value) { message_.append(value); }
- inline void SetLineString(const std::string& value) { message_ = value + CRLF; }
+  // Inline functions for Create Redis protocol
+  inline void AppendStringLen(int64_t ori) { RedisAppendLen(message_, ori, "$"); }
+  inline void AppendStringLenUint64(uint64_t ori) { RedisAppendLenUint64(message_, ori, "$"); }
+  inline void AppendArrayLen(int64_t ori) { RedisAppendLen(message_, ori, "*"); }
+  inline void AppendArrayLenUint64(uint64_t ori) { RedisAppendLenUint64(message_, ori, "*"); }
+  inline void AppendInteger(int64_t ori) { RedisAppendLen(message_, ori, ":"); }
+  inline void AppendContent(const std::string& value) { RedisAppendContent(message_, value); }
+  inline void AppendStringRaw(const std::string& value) { message_.append(value); }
+  inline void SetLineString(const std::string& value) { message_ = value + CRLF; }
 
- void AppendString(const std::string& value);
- void AppendStringVector(const std::vector<std::string>& strArray);
- void RedisAppendLenUint64(std::string& str, uint64_t ori, const std::string& prefix) {
-   RedisAppendLen(str, static_cast<int64_t>(ori), prefix);
- }
+  void AppendString(const std::string& value);
+  void AppendStringVector(const std::vector<std::string>& strArray);
+  void RedisAppendLenUint64(std::string& str, uint64_t ori, const std::string& prefix) {
+    RedisAppendLen(str, static_cast<int64_t>(ori), prefix);
+  }
 
- void SetRes(CmdRet _ret, const std::string& content = "");
+  void SetRes(CmdRet _ret, const std::string& content = "");
 
-protected:
- inline void RedisAppendContent(std::string& str, const std::string& value) {
-   str.append(value.data(), value.size());
-   str.append(CRLF);
- }
+ protected:
+  inline void RedisAppendContent(std::string& str, const std::string& value) {
+    str.append(value.data(), value.size());
+    str.append(CRLF);
+  }
 
- void RedisAppendLen(std::string& str, int64_t ori, const std::string& prefix);
+  void RedisAppendLen(std::string& str, int64_t ori, const std::string& prefix);
 
-private:
- std::string message_;
- CmdRet ret_ = kNone;
+ private:
+  std::string message_;
+  CmdRet ret_ = kNone;
 };
 
 enum ClientFlag {
- kClientFlagMulti = (1 << 0),
- kClientFlagDirty = (1 << 1),
- kClientFlagWrongExec = (1 << 2),
- kClientFlagMaster = (1 << 3),
+  kClientFlagMulti = (1 << 0),
+  kClientFlagDirty = (1 << 1),
+  kClientFlagWrongExec = (1 << 2),
+  kClientFlagMaster = (1 << 3),
 };
 
 class DB;
 struct PSlaveInfo;
 
 class PClient : public std::enable_shared_from_this<PClient>, public CmdRes {
-public:
- PClient() = delete;
- explicit PClient(TcpConnection* obj);
+ public:
+  PClient() = delete;
+  explicit PClient(TcpConnection* obj);
 
- int HandlePackets(pikiwidb::TcpConnection*, const char*, int);
+  int HandlePackets(pikiwidb::TcpConnection*, const char*, int);
 
- void OnConnect();
+  void OnConnect();
 
- const std::string& PeerIP() const;
- int PeerPort() const;
+  const std::string& PeerIP() const;
+  int PeerPort() const;
 
- bool SendPacket(const std::string& buf);
- bool SendPacket(const void* data, size_t size);
- bool SendPacket(UnboundedBuffer& data);
- bool SendPacket(const evbuffer_iovec* iovecs, size_t nvecs);
+  bool SendPacket(const std::string& buf);
+  bool SendPacket(const void* data, size_t size);
+  bool SendPacket(UnboundedBuffer& data);
+  bool SendPacket(const evbuffer_iovec* iovecs, size_t nvecs);
 
- void Close();
+  void Close();
 
- bool SelectDB(int db);
- static PClient* Current();
+  bool SelectDB(int db);
+  static PClient* Current();
 
- // multi
- void SetFlag(uint32_t flag) { flag_ |= flag; }
- void ClearFlag(uint32_t flag) { flag_ &= ~flag; }
- bool IsFlagOn(uint32_t flag) { return flag_ & flag; }
- void FlagExecWrong() {
-   if (IsFlagOn(kClientFlagMulti)) {
-     SetFlag(kClientFlagWrongExec);
-   }
- }
+  // multi
+  void SetFlag(uint32_t flag) { flag_ |= flag; }
+  void ClearFlag(uint32_t flag) { flag_ &= ~flag; }
+  bool IsFlagOn(uint32_t flag) { return flag_ & flag; }
+  void FlagExecWrong() {
+    if (IsFlagOn(kClientFlagMulti)) {
+      SetFlag(kClientFlagWrongExec);
+    }
+  }
 
- bool Watch(int dbno, const std::string& key);
- bool NotifyDirty(int dbno, const std::string& key);
- bool Exec();
- void ClearMulti();
- void ClearWatch();
+  bool Watch(int dbno, const std::string& key);
+  bool NotifyDirty(int dbno, const std::string& key);
+  bool Exec();
+  void ClearMulti();
+  void ClearWatch();
 
- // pubsub
- std::size_t Subscribe(const std::string& channel) { return channels_.insert(channel).second ? 1 : 0; }
+  // pubsub
+  std::size_t Subscribe(const std::string& channel) { return channels_.insert(channel).second ? 1 : 0; }
 
- std::size_t UnSubscribe(const std::string& channel) { return channels_.erase(channel); }
+  std::size_t UnSubscribe(const std::string& channel) { return channels_.erase(channel); }
 
- std::size_t PSubscribe(const std::string& channel) { return pattern_channels_.insert(channel).second ? 1 : 0; }
+  std::size_t PSubscribe(const std::string& channel) { return pattern_channels_.insert(channel).second ? 1 : 0; }
 
- std::size_t PUnSubscribe(const std::string& channel) { return pattern_channels_.erase(channel); }
+  std::size_t PUnSubscribe(const std::string& channel) { return pattern_channels_.erase(channel); }
 
- const std::unordered_set<std::string>& GetChannels() const { return channels_; }
- const std::unordered_set<std::string>& GetPatternChannels() const { return pattern_channels_; }
- std::size_t ChannelCount() const { return channels_.size(); }
- std::size_t PatternChannelCount() const { return pattern_channels_.size(); }
+  const std::unordered_set<std::string>& GetChannels() const { return channels_; }
+  const std::unordered_set<std::string>& GetPatternChannels() const { return pattern_channels_; }
+  std::size_t ChannelCount() const { return channels_.size(); }
+  std::size_t PatternChannelCount() const { return pattern_channels_.size(); }
 
- bool WaitFor(const std::string& key, const std::string* target = nullptr);
+  bool WaitFor(const std::string& key, const std::string* target = nullptr);
 
- std::unordered_set<std::string> WaitingKeys() const { return waiting_keys_; }
- void ClearWaitingKeys() { waiting_keys_.clear(), target_.clear(); }
- const std::string& GetTarget() const { return target_; }
+  std::unordered_set<std::string> WaitingKeys() const { return waiting_keys_; }
+  void ClearWaitingKeys() { waiting_keys_.clear(), target_.clear(); }
+  const std::string& GetTarget() const { return target_; }
 
- void SetName(const std::string& name) { name_ = name; }
- const std::string& GetName() const { return name_; }
- void SetCmdName(const std::string& name) { cmdName_ = name; }
- const std::string& CmdName() const { return cmdName_; }
- void SetSubCmdName(const std::string& name);
- const std::string& SubCmdName() const { return subCmdName_; }
- std::string FullCmdName() const;  // the full name of the command, such as config set|get|rewrite
- void SetKey(const std::string& name) {
-   keys_.clear();
-   keys_.emplace_back(name);
- }
- void SetKey(std::vector<std::string>& names);
- const std::string& Key() const { return keys_.at(0); }
- const std::vector<std::string>& Keys() const { return keys_; }
- std::vector<storage::FieldValue>& Fvs() { return fvs_; }
- void ClearFvs() { fvs_.clear(); }
- std::vector<std::string>& Fields() { return fields_; }
- void ClearFields() { fields_.clear(); }
+  void SetName(const std::string& name) { name_ = name; }
+  const std::string& GetName() const { return name_; }
+  void SetCmdName(const std::string& name) { cmdName_ = name; }
+  const std::string& CmdName() const { return cmdName_; }
+  void SetSubCmdName(const std::string& name);
+  const std::string& SubCmdName() const { return subCmdName_; }
+  std::string FullCmdName() const;  // the full name of the command, such as config set|get|rewrite
+  void SetKey(const std::string& name) {
+    keys_.clear();
+    keys_.emplace_back(name);
+  }
+  void SetKey(std::vector<std::string>& names);
+  const std::string& Key() const { return keys_.at(0); }
+  const std::vector<std::string>& Keys() const { return keys_; }
+  std::vector<storage::FieldValue>& Fvs() { return fvs_; }
+  void ClearFvs() { fvs_.clear(); }
+  std::vector<std::string>& Fields() { return fields_; }
+  void ClearFields() { fields_.clear(); }
 
- void SetSlaveInfo();
- PSlaveInfo* GetSlaveInfo() const { return slave_info_.get(); }
- void TransferToSlaveThreads();
+  void SetSlaveInfo();
+  PSlaveInfo* GetSlaveInfo() const { return slave_info_.get(); }
+  void TransferToSlaveThreads();
 
- static void AddCurrentToMonitor();
- static void FeedMonitors(const std::vector<std::string>& params);
+  static void AddCurrentToMonitor();
+  static void FeedMonitors(const std::vector<std::string>& params);
 
- void SetAuth() { auth_ = true; }
- bool GetAuth() const { return auth_; }
- void RewriteCmd(std::vector<std::string>& params) { parser_.SetParams(params); }
+  void SetAuth() { auth_ = true; }
+  bool GetAuth() const { return auth_; }
+  void RewriteCmd(std::vector<std::string>& params) { parser_.SetParams(params); }
 
- // All parameters of this command (including the command itself)
- // e.g：["set","key","value"]
- std::span<std::string> argv_;
+  // All parameters of this command (including the command itself)
+  // e.g：["set","key","value"]
+  std::span<std::string> argv_;
 
-private:
- std::shared_ptr<TcpConnection> getTcpConnection() const { return tcp_connection_.lock(); }
- int handlePacket(const char*, int);
- void executeCommand();
- int processInlineCmd(const char*, size_t, std::vector<std::string>&);
- void reset();
- bool isPeerMaster() const;
- int uniqueID() const;
+ private:
+  std::shared_ptr<TcpConnection> getTcpConnection() const { return tcp_connection_.lock(); }
+  int handlePacket(const char*, int);
+  void executeCommand();
+  int processInlineCmd(const char*, size_t, std::vector<std::string>&);
+  void reset();
+  bool isPeerMaster() const;
+  int uniqueID() const;
 
- // TcpConnection's life is undetermined, so use weak ptr for safety.
- std::weak_ptr<TcpConnection> tcp_connection_;
+  // TcpConnection's life is undetermined, so use weak ptr for safety.
+  std::weak_ptr<TcpConnection> tcp_connection_;
 
- PProtoParser parser_;
+  PProtoParser parser_;
 
- int db_ = -1;
+  int db_ = -1;
 
- std::unordered_set<std::string> channels_;
- std::unordered_set<std::string> pattern_channels_;
+  std::unordered_set<std::string> channels_;
+  std::unordered_set<std::string> pattern_channels_;
 
- uint32_t flag_;
- std::unordered_map<int32_t, std::unordered_set<std::string> > watch_keys_;
- std::vector<std::vector<std::string> > queue_cmds_;
+  uint32_t flag_;
+  std::unordered_map<int32_t, std::unordered_set<std::string> > watch_keys_;
+  std::vector<std::vector<std::string> > queue_cmds_;
 
- // blocked list
- std::unordered_set<std::string> waiting_keys_;
- std::string target_;
+  // blocked list
+  std::unordered_set<std::string> waiting_keys_;
+  std::string target_;
 
- // slave info from master view
- std::unique_ptr<PSlaveInfo> slave_info_;
+  // slave info from master view
+  std::unique_ptr<PSlaveInfo> slave_info_;
 
- // name
- std::string name_;        // client name
- std::string subCmdName_;  // suchAs config set|get|rewrite
- std::string cmdName_;     // suchAs config
- std::vector<std::string> keys_;
- std::vector<storage::FieldValue> fvs_;
- std::vector<std::string> fields_;
+  // name
+  std::string name_;        // client name
+  std::string subCmdName_;  // suchAs config set|get|rewrite
+  std::string cmdName_;     // suchAs config
+  std::vector<std::string> keys_;
+  std::vector<storage::FieldValue> fvs_;
+  std::vector<std::string> fields_;
 
- // All parameters of this command (including the command itself)
- // e.g：["set","key","value"]
- std::vector<std::string> params_;
- // auth
- bool auth_ = false;
- time_t last_auth_ = 0;
+  // All parameters of this command (including the command itself)
+  // e.g：["set","key","value"]
+  std::vector<std::string> params_;
+  // auth
+  bool auth_ = false;
+  time_t last_auth_ = 0;
 
- static thread_local PClient* s_current;
+  static thread_local PClient* s_current;
 };
 
 }  // namespace pikiwidb
--- src/cmd_table_manager.cc
+++ src/cmd_table_manager.cc
@@ -1,9 +1,9 @@
 /*
-* Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
-* This source code is licensed under the BSD-style license found in the
-* LICENSE file in the root directory of this source tree. An additional grant
-* of patent rights can be found in the PATENTS file in the same directory.
-*/
+ * Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #include "cmd_table_manager.h"
 #include <memory>
@@ -16,90 +16,90 @@
 namespace pikiwidb {
 
 #define ADD_COMMAND(cmd, argc)                                                      \
- do {                                                                              \
-   std::unique_ptr<BaseCmd> ptr = std::make_unique<cmd##Cmd>(kCmdName##cmd, argc); \
-   cmds_->insert(std::make_pair(kCmdName##cmd, std::move(ptr)));                   \
- } while (0)
+  do {                                                                              \
+    std::unique_ptr<BaseCmd> ptr = std::make_unique<cmd##Cmd>(kCmdName##cmd, argc); \
+    cmds_->insert(std::make_pair(kCmdName##cmd, std::move(ptr)));                   \
+  } while (0)
 
 CmdTableManager::CmdTableManager() {
- cmds_ = std::make_unique<CmdTable>();
- cmds_->reserve(300);
+  cmds_ = std::make_unique<CmdTable>();
+  cmds_->reserve(300);
 }
 
 void CmdTableManager::InitCmdTable() {
- std::unique_lock wl(mutex_);
+  std::unique_lock wl(mutex_);
 
- // admin
- auto configPtr = std::make_unique<CmdConfig>(kCmdNameConfig, -2);
- configPtr->AddSubCmd(std::make_unique<CmdConfigGet>("get", -3));
- configPtr->AddSubCmd(std::make_unique<CmdConfigSet>("set", -4));
- cmds_->insert(std::make_pair(kCmdNameConfig, std::move(configPtr)));
+  // admin
+  auto configPtr = std::make_unique<CmdConfig>(kCmdNameConfig, -2);
+  configPtr->AddSubCmd(std::make_unique<CmdConfigGet>("get", -3));
+  configPtr->AddSubCmd(std::make_unique<CmdConfigSet>("set", -4));
+  cmds_->insert(std::make_pair(kCmdNameConfig, std::move(configPtr)));
 
- // server
- ADD_COMMAND(Flushdb, 1);
+  // server
+  ADD_COMMAND(Flushdb, 1);
 
- // keyspace
- ADD_COMMAND(Del, -2);
- ADD_COMMAND(Exists, 2);
+  // keyspace
+  ADD_COMMAND(Del, -2);
+  ADD_COMMAND(Exists, 2);
 
- // kv
- ADD_COMMAND(Get, 2);
- ADD_COMMAND(Set, -3);
- ADD_COMMAND(MGet, -2);
- ADD_COMMAND(MSet, -3);
- ADD_COMMAND(GetSet, 3);
- ADD_COMMAND(SetNX, 3);
- ADD_COMMAND(Append, 3);
- ADD_COMMAND(Strlen, 2);
- ADD_COMMAND(Incr, 2);
- ADD_COMMAND(Incrby, 3);
- ADD_COMMAND(Decrby, 3);
- ADD_COMMAND(IncrbyFloat, 3);
- ADD_COMMAND(SetEx, 4);
- ADD_COMMAND(PSetEx, 4);
- ADD_COMMAND(BitOp, -4);
- ADD_COMMAND(BitCount, -2);
- ADD_COMMAND(GetBit, 3);
- ADD_COMMAND(GetRange, 4);
- ADD_COMMAND(Decr, 2);
- ADD_COMMAND(SetBit, 4);
+  // kv
+  ADD_COMMAND(Get, 2);
+  ADD_COMMAND(Set, -3);
+  ADD_COMMAND(MGet, -2);
+  ADD_COMMAND(MSet, -3);
+  ADD_COMMAND(GetSet, 3);
+  ADD_COMMAND(SetNX, 3);
+  ADD_COMMAND(Append, 3);
+  ADD_COMMAND(Strlen, 2);
+  ADD_COMMAND(Incr, 2);
+  ADD_COMMAND(Incrby, 3);
+  ADD_COMMAND(Decrby, 3);
+  ADD_COMMAND(IncrbyFloat, 3);
+  ADD_COMMAND(SetEx, 4);
+  ADD_COMMAND(PSetEx, 4);
+  ADD_COMMAND(BitOp, -4);
+  ADD_COMMAND(BitCount, -2);
+  ADD_COMMAND(GetBit, 3);
+  ADD_COMMAND(GetRange, 4);
+  ADD_COMMAND(Decr, 2);
+  ADD_COMMAND(SetBit, 4);
 
- // hash
- ADD_COMMAND(HSet, -4);
- ADD_COMMAND(HGet, 3);
- ADD_COMMAND(HMSet, -4);
- ADD_COMMAND(HMGet, -3);
- ADD_COMMAND(HGetAll, 2);
- ADD_COMMAND(HKeys, 2);
- ADD_COMMAND(HLen, 2);
- ADD_COMMAND(HStrLen, 3);
+  // hash
+  ADD_COMMAND(HSet, -4);
+  ADD_COMMAND(HGet, 3);
+  ADD_COMMAND(HMSet, -4);
+  ADD_COMMAND(HMGet, -3);
+  ADD_COMMAND(HGetAll, 2);
+  ADD_COMMAND(HKeys, 2);
+  ADD_COMMAND(HLen, 2);
+  ADD_COMMAND(HStrLen, 3);
 
- // set
- ADD_COMMAND(SIsMember, 3);
- ADD_COMMAND(SAdd, -3);
+  // set
+  ADD_COMMAND(SIsMember, 3);
+  ADD_COMMAND(SAdd, -3);
 }
 
 std::pair<BaseCmd*, CmdRes::CmdRet> CmdTableManager::GetCommand(const std::string& cmdName, PClient* client) {
- std::shared_lock rl(mutex_);
+  std::shared_lock rl(mutex_);
 
- auto cmd = cmds_->find(cmdName);
+  auto cmd = cmds_->find(cmdName);
 
- if (cmd == cmds_->end()) {
-   return std::pair(nullptr, CmdRes::kSyntaxErr);
- }
+  if (cmd == cmds_->end()) {
+    return std::pair(nullptr, CmdRes::kSyntaxErr);
+  }
 
- if (cmd->second->HasSubCommand()) {
-   if (client->argv_.size() < 2) {
-     return std::pair(nullptr, CmdRes::kInvalidParameter);
-   }
-   return std::pair(cmd->second->GetSubCmd(client->argv_[1]), CmdRes::kSyntaxErr);
- }
- return std::pair(cmd->second.get(), CmdRes::kSyntaxErr);
+  if (cmd->second->HasSubCommand()) {
+    if (client->argv_.size() < 2) {
+      return std::pair(nullptr, CmdRes::kInvalidParameter);
+    }
+    return std::pair(cmd->second->GetSubCmd(client->argv_[1]), CmdRes::kSyntaxErr);
+  }
+  return std::pair(cmd->second.get(), CmdRes::kSyntaxErr);
 }
 
 bool CmdTableManager::CmdExist(const std::string& cmd) const {
- std::shared_lock rl(mutex_);
- return cmds_->find(cmd) != cmds_->end();
+  std::shared_lock rl(mutex_);
+  return cmds_->find(cmd) != cmds_->end();
 }
 
 uint32_t CmdTableManager::GetCmdId() { return ++cmdId_; }
