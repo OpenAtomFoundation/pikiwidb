d5ddcbed520cc7601246c5a6cf54d8af289171f4
Korpse <543514071@qq.com>
2024-01-08 23:10:26 +0800

    feat: add basic braft state_machine class.

--- cmake/leveldb.cmake
+++ cmake/leveldb.cmake
@@ -1,19 +1,18 @@
-# Copyright (c) 2023-present, Qihoo, Inc.  All rights reserved.
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
-
-include_guard()
-
-FETCHCONTENT_DECLARE(
-        leveldb
-        GIT_REPOSITORY https://github.com/google/leveldb.git
-        GIT_TAG main
+FetchContent_Declare(
+    leveldb
+    GIT_REPOSITORY https://github.com/google/leveldb.git
+    GIT_TAG main
 )
-SET(LEVELDB_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-SET(LEVELDB_BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
-SET(LEVELDB_INSTALL OFF CACHE BOOL "" FORCE)
-FETCHCONTENT_MAKEAVAILABLE(leveldb)
+
+# 检查 leveldb 是否已经被下载
+FetchContent_GetProperties(leveldb)
+if(NOT leveldb_POPULATED)
+    FetchContent_Populate(leveldb)
+    SET(LEVELDB_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    SET(LEVELDB_BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
+    SET(LEVELDB_INSTALL OFF CACHE BOOL "" FORCE)
+    add_subdirectory(${leveldb_SOURCE_DIR} ${leveldb_BINARY_DIR})
+endif()
 
 SET(LEVELDB_INCLUDE_PATH ${CMAKE_CURRENT_BINARY_DIR}/_deps/leveldb-build/include)
 SET(LEVELDB_LIB ${CMAKE_CURRENT_BINARY_DIR}/_deps/leveldb-build/libleveldb.a)
