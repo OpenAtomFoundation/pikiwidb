0f1eadae132d9aea59718b0ae56f62daf4dcaada
panlei-coder <panleiCSDN@163.com>
2024-01-09 23:39:54 +0800

    fix: fix braft.cmake

--- cmake/brpc.cmake
+++ cmake/brpc.cmake
@@ -3,6 +3,9 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
+if (CMAKE_VERSION VERSION_GREATER_EQUAL "2.8.12")
+  cmake_policy(SET CMP0135 NEW)
+endif()
 
 include(cmake/gflags.cmake)
 include(cmake/protobuf.cmake)
@@ -15,12 +18,16 @@ FETCHCONTENT_DECLARE(
         # SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/brpc-src
         # BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/brpc-build/output/include
 )
-set(CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR}/build/_deps/brpc-build/output ${CMAKE_PREFIX_PATH})
-# set(BRPC_DIR ${CMAKE_CURRENT_BINARY_DIR}/build/_deps/brpc-build/output/include)
+
+set(BRPC_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/brpc-build)
+set(CMAKE_PREFIX_PATH ${BRPC_BUILD_DIR}/output ${CMAKE_PREFIX_PATH})
 
 SET(BRPC_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 SET(BRPC_BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
 SET(BRPC_INSTALL OFF CACHE BOOL "" FORCE)
 FETCHCONTENT_MAKEAVAILABLE(brpc)
 
+add_library(brpc STATIC IMPORTED)
+set_target_properties(brpc PROPERTIES IMPORTED_LOCATION ${BRPC_BUILD_DIR}/output/lib/libbrpc.a)
 # target_link_libraries(brpc gflags_static protobuf leveldb)
+message(${BRPC_BUILD_DIR})
