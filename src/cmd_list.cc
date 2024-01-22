d778c097d33b07a582b06fd95bdd8c2a873787ca
swx <cwl990110@163.com>
2024-01-22 13:05:29 +0800

    format

--- src/cmd_list.cc
+++ src/cmd_list.cc
@@ -6,20 +6,18 @@
  */
 
 #include "cmd_list.h"
-
-#include <iostream>
-#include <memory>
-#include <utility>
 #include "store.h"
 
 namespace pikiwidb {
 
 LPushCmd::LPushCmd(const std::string& name, int16_t arity)
     : BaseCmd(name, arity, kCmdFlagsWrite, kAclCategoryWrite | kAclCategoryList) {}
+
 bool LPushCmd::DoInitial(PClient* client) {
   client->SetKey(client->argv_[1]);
   return true;
 }
+
 void LPushCmd::DoCmd(PClient* client) {
   std::vector<std::string> list_values(client->argv_.begin() + 2, client->argv_.end());
   uint64_t reply_num = 0;
@@ -30,12 +28,15 @@ void LPushCmd::DoCmd(PClient* client) {
     client->SetRes(CmdRes::kSyntaxErr, "lpush cmd error");
   }
 }
+
 RPushCmd::RPushCmd(const std::string& name, int16_t arity)
     : BaseCmd(name, arity, kCmdFlagsWrite, kAclCategoryWrite | kAclCategoryList) {}
+
 bool RPushCmd::DoInitial(PClient* client) {
   client->SetKey(client->argv_[1]);
   return true;
 }
+
 void RPushCmd::DoCmd(PClient* client) {
   std::vector<std::string> list_values(client->argv_.begin() + 2, client->argv_.end());
   uint64_t reply_num = 0;
