eb12cd8f563f97232144178f99d180c8f1cf8b08
super-Pan66 <1719739132@qq.com>
2024-02-21 20:09:22 +0800

    handle conflicts

--- src/cmd_hash.cc
+++ src/cmd_hash.cc
@@ -258,8 +258,6 @@ bool HScanCmd::DoInitial(PClient* client) {
     client->SetRes(CmdRes::kSyntaxErr);
     return false;
   }
-  client->SetKey(client->argv_[1]);
-  return true;
 }
 
 void HScanCmd::DoCmd(PClient* client) {
@@ -325,7 +323,7 @@ void HValsCmd::DoCmd(PClient* client) {
   }
 }
 
-HIncrbyFloatCmd::HIncrbyFloatCmd(const std::string& name, int16_t arity)
+HIncrbyFloatCmd::HIncrbyFloatCmd(const std::string &name, int16_t arity)
     : BaseCmd(name, arity, kCmdFlagsWrite, kAclCategoryWrite | kAclCategoryHash) {}
 
 bool HIncrbyFloatCmd::DoInitial(PClient* client) {
@@ -340,7 +338,7 @@ bool HIncrbyFloatCmd::DoInitial(PClient* client) {
 
 void HIncrbyFloatCmd::DoCmd(PClient* client) {
   long double long_double_by = 0;
-  if (-1 == StrToLongDouble(client->argv_[3].c_str(), static_cast<int>(client->argv_[3].size()), &long_double_by)) {
+  if(-1 == StrToLongDouble(client->argv_[3].c_str(), static_cast<int>(client->argv_[3].size()), &long_double_by)){
     client->SetRes(CmdRes::kInvalidFloat);
     return;
   }
