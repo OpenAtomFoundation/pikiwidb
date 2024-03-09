b6637dec724e812d4882e3aefdbc74dd50a0af4b
longfar <ningchangyuan@nuaa.edu.cn>
2024-03-09 23:44:59 +0800

    fix: an unknown bug in master branch of braft causes failure of leader to add follower

--- cmake/braft.cmake
+++ cmake/braft.cmake
@@ -16,8 +16,8 @@ ExternalProject_Add(
         extern_braft
         ${EXTERNAL_PROJECT_LOG_ARGS}
         DEPENDS brpc
-        GIT_REPOSITORY https://github.com/baidu/braft.git
-        GIT_TAG master
+        URL "https://github.com/baidu/braft/archive/v1.1.2.tar.gz"
+        URL_HASH SHA256=bb3705f61874f8488e616ae38464efdec1a20610ddd6cd82468adc814488f14e
         PREFIX ${BRAFT_SOURCES_DIR}
         UPDATE_COMMAND ""
         CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
