OpenAtomFoundation/pika

feat:incr send binlog withttl (#2833)
Co-authored-by: chejinge <chejinge@360.cn>

include/pika_kv.h
@@ -115,6 +115,8 @@ class IncrCmd : public Cmd {
   int64_t new_value_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int64_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class IncrbyCmd : public Cmd {
@@ -138,6 +140,8 @@ class IncrbyCmd : public Cmd {
   int64_t by_ = 0, new_value_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int64_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class IncrbyfloatCmd : public Cmd {
@@ -161,6 +165,8 @@ class IncrbyfloatCmd : public Cmd {
   double by_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int64_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class DecrCmd : public Cmd {
@@ -251,8 +257,11 @@ class AppendCmd : public Cmd {
  private:
   std::string key_;
   std::string value_;
+  std::string new_value_;
   void DoInitial() override;
   rocksdb::Status s_;
+  int64_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class MgetCmd : public Cmd {

src/storage/include/storage/storage.h
@@ -260,7 +260,7 @@ class Storage {
   // If key already exists and is a string, this command appends the value at
   // the end of the string
   // return the length of the string after the append operation
-  Status Append(const Slice& key, const Slice& value, int32_t* ret);
+  Status Append(const Slice& key, const Slice& value, int32_t* ret, int64_t* expired_timestamp_sec, std::string& out_new_value);
 
   // Count the number of set bits (population counting) in a string.
   // return the number of bits set to 1
@@ -285,11 +285,11 @@ class Storage {
 
   // Increments the number stored at key by increment.
   // If the key does not exist, it is set to 0 before performing the operation
-  Status Incrby(const Slice& key, int64_t value, int64_t* ret);
+  Status Incrby(const Slice& key, int64_t value, int64_t* ret, int64_t* expired_timestamp_sec);
 
   // Increment the string representing a floating point number
   // stored at key by the specified increment.
-  Status Incrbyfloat(const Slice& key, const Slice& value, std::string* ret);
+  Status Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int64_t* expired_timestamp_sec);
 
   // Set key to hold the string value and set key to timeout after a given
   // number of seconds

src/storage/src/redis_strings.cc
@@ -63,9 +63,10 @@ Status Redis::ScanStringsKeyNum(KeyInfo* key_info) {
   return Status::OK();
 }
 
-Status Redis::Append(const Slice& key, const Slice& value, int32_t* ret) {
+Status Redis::Append(const Slice& key, const Slice& value, int32_t* ret, int64_t* expired_timestamp_sec, std::string& out_new_value) {
   std::string old_value;
   *ret = 0;
+  *expired_timestamp_sec = 0;
   ScopeRecordLock l(lock_mgr_, key);
 
   BaseKey base_key(key);
@@ -90,14 +91,17 @@ Status Redis::Append(const Slice& key, const Slice& value, int32_t* ret) {
       uint64_t timestamp = parsed_strings_value.Etime();
       std::string old_user_value = parsed_strings_value.UserValue().ToString();
       std::string new_value = old_user_value + value.ToString();
+      out_new_value = new_value;
       StringsValue strings_value(new_value);
       strings_value.SetEtime(timestamp);
       *ret = static_cast<int32_t>(new_value.size());
+      *expired_timestamp_sec = timestamp;
       return db_->Put(default_write_options_, base_key.Encode(), strings_value.Encode());
     }
   } else if (s.IsNotFound()) {
     *ret = static_cast<int32_t>(value.size());
     StringsValue strings_value(value);
+    *expired_timestamp_sec = 0;
     return db_->Put(default_write_options_, base_key.Encode(), strings_value.Encode());
   }
   return s;
@@ -618,11 +622,10 @@ Status Redis::GetSet(const Slice& key, const Slice& value, std::string* old_valu
   return db_->Put(default_write_options_, base_key.Encode(), strings_value.Encode());
 }
 
-Status Redis::Incrby(const Slice& key, int64_t value, int64_t* ret) {
+Status Redis::Incrby(const Slice& key, int64_t value, int64_t* ret, int64_t* expired_timestamp_sec) {
   std::string old_value;
   std::string new_value;
   ScopeRecordLock l(lock_mgr_, key);
-
   BaseKey base_key(key);
   Status s = db_->Get(default_read_options_, base_key.Encode(), &old_value);
   char buf[32] = {0};
@@ -630,10 +633,9 @@ Status Redis::Incrby(const Slice& key, int64_t value, int64_t* ret) {
     if (ExpectedStale(old_value)) {
       s = Status::NotFound();
     } else {
-     return Status::InvalidArgument(
-        "WRONGTYPE, key: " + key.ToString() + ", expect type: " +
-        DataTypeStrings[static_cast<int>(DataType::kStrings)] + ", get type: " +
-        DataTypeStrings[static_cast<int>(GetMetaValueType(old_value))]);
+      return Status::InvalidArgument("WRONGTYPE, key: " + key.ToString() +
+                                     ", expect type: " + DataTypeStrings[static_cast<int>(DataType::kStrings)] +
+                                     ", get type: " + DataTypeStrings[static_cast<int>(GetMetaValueType(old_value))]);
     }
   }
   if (s.ok()) {
@@ -658,6 +660,7 @@ Status Redis::Incrby(const Slice& key, int64_t value, int64_t* ret) {
       new_value = std::to_string(*ret);
       StringsValue strings_value(new_value);
       strings_value.SetEtime(timestamp);
+      *expired_timestamp_sec = timestamp;
       return db_->Put(default_write_options_, base_key.Encode(), strings_value.Encode());
     }
   } else if (s.IsNotFound()) {
@@ -670,9 +673,10 @@ Status Redis::Incrby(const Slice& key, int64_t value, int64_t* ret) {
   }
 }
 
-Status Redis::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret) {
+Status Redis::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int64_t* expired_timestamp_sec) {
   std::string old_value;
   std::string new_value;
+  *expired_timestamp_sec = 0;
   long double long_double_by;
   if (StrToLongDouble(value.data(), value.size(), &long_double_by) == -1) {
     return Status::Corruption("Value is not a vaild float");
@@ -713,6 +717,7 @@ Status Redis::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret
       *ret = new_value;
       StringsValue strings_value(new_value);
       strings_value.SetEtime(timestamp);
+      *expired_timestamp_sec = timestamp;
       return db_->Put(default_write_options_, base_key.Encode(), strings_value.Encode());
     }
   } else if (s.IsNotFound()) {

src/storage/src/storage.cc
@@ -283,9 +283,9 @@ Status Storage::GetrangeWithValue(const Slice& key, int64_t start_offset, int64_
   return inst->GetrangeWithValue(key, start_offset, end_offset, ret, value, ttl);
 }
 
-Status Storage::Append(const Slice& key, const Slice& value, int32_t* ret) {
+Status Storage::Append(const Slice& key, const Slice& value, int32_t* ret, int64_t* expired_timestamp_sec, std::string& out_new_value) {
   auto& inst = GetDBInstance(key);
-  return inst->Append(key, value, ret);
+  return inst->Append(key, value, ret, expired_timestamp_sec, out_new_value);
 }
 
 Status Storage::BitCount(const Slice& key, int64_t start_offset, int64_t end_offset, int32_t* ret, bool have_range) {
@@ -347,14 +347,14 @@ Status Storage::Decrby(const Slice& key, int64_t value, int64_t* ret) {
   return inst->Decrby(key, value, ret);
 }
 
-Status Storage::Incrby(const Slice& key, int64_t value, int64_t* ret) {
+Status Storage::Incrby(const Slice& key, int64_t value, int64_t* ret, int64_t* expired_timestamp_sec) {
   auto& inst = GetDBInstance(key);
-  return inst->Incrby(key, value, ret);
+  return inst->Incrby(key, value, ret, expired_timestamp_sec);
 }
 
-Status Storage::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret) {
+Status Storage::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int64_t* expired_timestamp_sec) {
   auto& inst = GetDBInstance(key);
-  return inst->Incrbyfloat(key, value, ret);
+  return inst->Incrbyfloat(key, value, ret, expired_timestamp_sec);
 }
 
 Status Storage::Setex(const Slice& key, const Slice& value, int64_t ttl) {

src/storage/tests/strings_test.cc
@@ -69,14 +69,15 @@ static bool string_ttl(storage::Storage* const db, const Slice& key, int32_t* tt
 TEST_F(StringsTest, AppendTest) {
   int32_t ret;
   std::string value;
+  std::string new_value;
   std::map<DataType, Status> type_status;
   int64_t type_ttl;
   // ***************** Group 1 Test *****************
-  s = db.Append("GP1_APPEND_KEY", "HELLO", &ret);
+  s = db.Append("GP1_APPEND_KEY", "HELLO", &ret, &type_ttl, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
 
-  s = db.Append("GP1_APPEND_KEY", " WORLD", &ret);
+  s = db.Append("GP1_APPEND_KEY", " WORLD", &ret, &type_ttl, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 11);
 
@@ -93,7 +94,7 @@ TEST_F(StringsTest, AppendTest) {
   ASSERT_LE(type_ttl, 100);
   ASSERT_GE(type_ttl, 0);
 
-  s = db.Append("GP2_APPEND_KEY", "VALUE", &ret);
+  s = db.Append("GP2_APPEND_KEY", "VALUE", &ret, &type_ttl, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 10);
   s = db.Get("GP2_APPEND_KEY", &value);
@@ -109,7 +110,7 @@ TEST_F(StringsTest, AppendTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_APPEND_KEY");
 
-  s = db.Append("GP3_APPEND_KEY", "VALUE", &ret);
+  s = db.Append("GP3_APPEND_KEY", "VALUE", &ret, &type_ttl, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
   s = db.Get("GP3_APPEND_KEY", &value);
@@ -353,20 +354,20 @@ TEST_F(StringsTest, IncrbyTest) {
 
   // ***************** Group 1 Test *****************
   // If the key is not exist
-  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
 
   // If the key contains a string that can not be represented as integer
   s = db.Set("GP1_INCRBY_KEY", "INCRBY_VALUE");
   ASSERT_TRUE(s.ok());
-  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret, &type_ttl);
   ASSERT_TRUE(s.IsCorruption());
 
   s = db.Set("GP1_INCRBY_KEY", "1");
   ASSERT_TRUE(s.ok());
   // Less than the maximum number 9223372036854775807
-  s = db.Incrby("GP1_INCRBY_KEY", 9223372036854775807, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 9223372036854775807, &ret, &type_ttl);
   ASSERT_TRUE(s.IsInvalidArgument());
 
   // ***************** Group 2 Test *****************
@@ -379,7 +380,7 @@ TEST_F(StringsTest, IncrbyTest) {
   ASSERT_LE(type_ttl, 100);
   ASSERT_GE(type_ttl, 0);
 
-  s = db.Incrby("GP2_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP2_INCRBY_KEY", 5, &ret, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 15);
   s = db.Get("GP2_INCRBY_KEY", &value);
@@ -394,7 +395,7 @@ TEST_F(StringsTest, IncrbyTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_INCRBY_KEY");
 
-  s = db.Incrby("GP3_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP3_INCRBY_KEY", 5, &ret, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
   s = db.Get("GP3_INCRBY_KEY", &value);
@@ -408,7 +409,7 @@ TEST_F(StringsTest, IncrbyTest) {
   s = db.Set("GP4_INCRBY_KEY", "50000");
   ASSERT_TRUE(s.ok());
 
-  s = db.Incrby("GP4_INCRBY_KEY", 50000, &ret);
+  s = db.Incrby("GP4_INCRBY_KEY", 50000, &ret, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 100000);
   s = db.Get("GP4_INCRBY_KEY", &value);
@@ -428,17 +429,17 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   // ***************** Group 1 Test *****************
   s = db.Set("GP1_INCRBYFLOAT_KEY", "10.50");
   ASSERT_TRUE(s.ok());
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "0.1", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "0.1", &value, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 10.6, eps);
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "-5", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "-5", &value, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 5.6, eps);
 
   // If the key contains a string that can not be represented as integer
   s = db.Set("GP1_INCRBYFLOAT_KEY", "INCRBY_VALUE");
   ASSERT_TRUE(s.ok());
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "5", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "5", &value, &type_ttl);
   ASSERT_TRUE(s.IsCorruption());
 
   // ***************** Group 2 Test *****************
@@ -451,7 +452,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   ASSERT_LE(type_ttl, 100);
   ASSERT_GE(type_ttl, 0);
 
-  s = db.Incrbyfloat("GP2_INCRBYFLOAT_KEY", "10.22222", &value);
+  s = db.Incrbyfloat("GP2_INCRBYFLOAT_KEY", "10.22222", &value, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 20.33333, eps);
   s = db.Get("GP2_INCRBYFLOAT_KEY", &value);
@@ -466,7 +467,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_INCRBYFLOAT_KEY");
 
-  s = db.Incrbyfloat("GP3_INCRBYFLOAT_KEY", "0.123456", &value);
+  s = db.Incrbyfloat("GP3_INCRBYFLOAT_KEY", "0.123456", &value, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 0.123456, eps);
   s = db.Get("GP3_INCRBYFLOAT_KEY", &value);
@@ -480,7 +481,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   s = db.Set("GP4_INCRBYFLOAT_KEY", "100.001");
   ASSERT_TRUE(s.ok());
 
-  s = db.Incrbyfloat("GP4_INCRBYFLOAT_KEY", "11.11", &value);
+  s = db.Incrbyfloat("GP4_INCRBYFLOAT_KEY", "11.11", &value, &type_ttl);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 111.111, eps);
   s = db.Get("GP4_INCRBYFLOAT_KEY", &value);

OpenAtomFoundation/pikiwidb

include/pika_kv.h
@@ -115,6 +115,8 @@ class IncrCmd : public Cmd {
   int64_t new_value_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int32_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class IncrbyCmd : public Cmd {
@@ -138,6 +140,8 @@ class IncrbyCmd : public Cmd {
   int64_t by_ = 0, new_value_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int32_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class IncrbyfloatCmd : public Cmd {
@@ -161,6 +165,8 @@ class IncrbyfloatCmd : public Cmd {
   double by_ = 0;
   void DoInitial() override;
   rocksdb::Status s_;
+  int32_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class DecrCmd : public Cmd {
@@ -251,8 +257,11 @@ class AppendCmd : public Cmd {
  private:
   std::string key_;
   std::string value_;
+  std::string new_value_;
   void DoInitial() override;
   rocksdb::Status s_;
+  int32_t expired_timestamp_sec_ = 0;
+  std::string ToRedisProtocol() override;
 };
 
 class MgetCmd : public Cmd {

src/storage/include/storage/storage.h
@@ -251,7 +251,7 @@ class Storage {
   // If key already exists and is a string, this command appends the value at
   // the end of the string
   // return the length of the string after the append operation
-  Status Append(const Slice& key, const Slice& value, int32_t* ret);
+  Status Append(const Slice& key, const Slice& value, int32_t* ret, int32_t* expired_timestamp_sec, std::string& out_new_value);
 
   // Count the number of set bits (population counting) in a string.
   // return the number of bits set to 1
@@ -276,11 +276,11 @@ class Storage {
 
   // Increments the number stored at key by increment.
   // If the key does not exist, it is set to 0 before performing the operation
-  Status Incrby(const Slice& key, int64_t value, int64_t* ret);
+  Status Incrby(const Slice& key, int64_t value, int64_t* ret, int32_t* expired_timestamp_sec);
 
   // Increment the string representing a floating point number
   // stored at key by the specified increment.
-  Status Incrbyfloat(const Slice& key, const Slice& value, std::string* ret);
+  Status Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int32_t* expired_timestamp_sec);
 
   // Set key to hold the string value and set key to timeout after a given
   // number of seconds

src/storage/src/redis_strings.cc
@@ -156,9 +156,10 @@ Status RedisStrings::PKPatternMatchDelWithRemoveKeys(const DataType& data_type, 
   return s;
 }
 
-Status RedisStrings::Append(const Slice& key, const Slice& value, int32_t* ret) {
+Status RedisStrings::Append(const Slice& key, const Slice& value, int32_t* ret, int32_t* expired_timestamp_sec, std::string& out_new_value) {
   std::string old_value;
   *ret = 0;
+  *expired_timestamp_sec = 0;
   ScopeRecordLock l(lock_mgr_, key);
   Status s = db_->Get(default_read_options_, key, &old_value);
   if (s.ok()) {
@@ -171,15 +172,18 @@ Status RedisStrings::Append(const Slice& key, const Slice& value, int32_t* ret) 
       int32_t timestamp = parsed_strings_value.timestamp();
       std::string old_user_value = parsed_strings_value.value().ToString();
       std::string new_value = old_user_value + value.ToString();
+      out_new_value = new_value;
       StringsValue strings_value(new_value);
       strings_value.set_timestamp(timestamp);
       *ret = static_cast<int32_t>(new_value.size());
       return db_->Put(default_write_options_, key, strings_value.Encode());
+      *expired_timestamp_sec = timestamp;
     }
   } else if (s.IsNotFound()) {
     *ret = static_cast<int32_t>(value.size());
     StringsValue strings_value(value);
     return db_->Put(default_write_options_, key, strings_value.Encode());
+    *expired_timestamp_sec = 0;
   }
   return s;
 }
@@ -539,9 +543,10 @@ Status RedisStrings::GetSet(const Slice& key, const Slice& value, std::string* o
   return db_->Put(default_write_options_, key, strings_value.Encode());
 }
 
-Status RedisStrings::Incrby(const Slice& key, int64_t value, int64_t* ret) {
+Status RedisStrings::Incrby(const Slice& key, int64_t value, int64_t* ret, int32_t* expired_timestamp_sec) {
   std::string old_value;
   std::string new_value;
+  *expired_timestamp_sec = 0;
   ScopeRecordLock l(lock_mgr_, key);
   Status s = db_->Get(default_read_options_, key, &old_value);
   char buf[32] = {0};
@@ -568,20 +573,23 @@ Status RedisStrings::Incrby(const Slice& key, int64_t value, int64_t* ret) {
       StringsValue strings_value(new_value);
       strings_value.set_timestamp(timestamp);
       return db_->Put(default_write_options_, key, strings_value.Encode());
+      *expired_timestamp_sec = timestamp;
     }
   } else if (s.IsNotFound()) {
     *ret = value;
     Int64ToStr(buf, 32, value);
     StringsValue strings_value(buf);
+    *expired_timestamp_sec = 0;
     return db_->Put(default_write_options_, key, strings_value.Encode());
   } else {
     return s;
   }
 }
 
-Status RedisStrings::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret) {
+Status RedisStrings::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int32_t* expired_timestamp_sec) {
   std::string old_value;
   std::string new_value;
+  *expired_timestamp_sec = 0;
   long double long_double_by;
   if (StrToLongDouble(value.data(), value.size(), &long_double_by) == -1) {
     return Status::Corruption("Value is not a vaild float");
@@ -611,11 +619,13 @@ Status RedisStrings::Incrbyfloat(const Slice& key, const Slice& value, std::stri
       StringsValue strings_value(new_value);
       strings_value.set_timestamp(timestamp);
       return db_->Put(default_write_options_, key, strings_value.Encode());
+      *expired_timestamp_sec = timestamp;
     }
   } else if (s.IsNotFound()) {
     LongDoubleToStr(long_double_by, &new_value);
     *ret = new_value;
     StringsValue strings_value(new_value);
+    *expired_timestamp_sec = 0;
     return db_->Put(default_write_options_, key, strings_value.Encode());
   } else {
     return s;

src/storage/src/storage.cc
@@ -200,8 +200,8 @@ Status Storage::GetrangeWithValue(const Slice& key, int64_t start_offset, int64_
   return strings_db_->GetrangeWithValue(key, start_offset, end_offset, ret, value, ttl);
 }
 
-Status Storage::Append(const Slice& key, const Slice& value, int32_t* ret) {
-  return strings_db_->Append(key, value, ret);
+Status Storage::Append(const Slice& key, const Slice& value, int32_t* ret, int32_t* expired_timestamp_sec, std::string& out_new_value) {
+  return strings_db_->Append(key, value, ret, expired_timestamp_sec, out_new_value);
 }
 
 Status Storage::BitCount(const Slice& key, int64_t start_offset, int64_t end_offset, int32_t* ret, bool have_range) {
@@ -225,10 +225,13 @@ Status Storage::BitPos(const Slice& key, int32_t bit, int64_t start_offset, int6
 
 Status Storage::Decrby(const Slice& key, int64_t value, int64_t* ret) { return strings_db_->Decrby(key, value, ret); }
 
-Status Storage::Incrby(const Slice& key, int64_t value, int64_t* ret) { return strings_db_->Incrby(key, value, ret); }
 
-Status Storage::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret) {
-  return strings_db_->Incrbyfloat(key, value, ret);
+Status Storage::Incrby(const Slice& key, int64_t value, int64_t* ret, int32_t* expired_timestamp_sec) {
+  return strings_db_->Incrby(key, value, ret, expired_timestamp_sec);
+}
+
+Status Storage::Incrbyfloat(const Slice& key, const Slice& value, std::string* ret, int32_t* expired_timestamp_sec) {
+  return strings_db_->Incrbyfloat(key, value, ret, expired_timestamp_sec);
 }
 
 Status Storage::Setex(const Slice& key, const Slice& value, int32_t ttl) { return strings_db_->Setex(key, value, ttl); }

src/storage/tests/strings_test.cc
@@ -71,14 +71,16 @@ static bool string_ttl(storage::Storage* const db, const Slice& key, int32_t* tt
 TEST_F(StringsTest, AppendTest) {
   int32_t ret;
   std::string value;
+  std::string new_value;
   std::map<DataType, Status> type_status;
   std::map<DataType, int64_t> type_ttl;
+  int32_t expired_timestamp_sec;
   // ***************** Group 1 Test *****************
-  s = db.Append("GP1_APPEND_KEY", "HELLO", &ret);
+  s = db.Append("GP1_APPEND_KEY", "HELLO", &ret, &expired_timestamp_sec, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
 
-  s = db.Append("GP1_APPEND_KEY", " WORLD", &ret);
+  s = db.Append("GP1_APPEND_KEY", " WORLD", &ret, &expired_timestamp_sec, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 11);
 
@@ -95,7 +97,7 @@ TEST_F(StringsTest, AppendTest) {
   ASSERT_LE(type_ttl[kStrings], 100);
   ASSERT_GE(type_ttl[kStrings], 0);
 
-  s = db.Append("GP2_APPEND_KEY", "VALUE", &ret);
+  s = db.Append("GP2_APPEND_KEY", "VALUE", &ret, &expired_timestamp_sec, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 10);
   s = db.Get("GP2_APPEND_KEY", &value);
@@ -111,7 +113,7 @@ TEST_F(StringsTest, AppendTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_APPEND_KEY");
 
-  s = db.Append("GP3_APPEND_KEY", "VALUE", &ret);
+  s = db.Append("GP3_APPEND_KEY", "VALUE", &ret, &expired_timestamp_sec, new_value);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
   s = db.Get("GP3_APPEND_KEY", &value);
@@ -351,23 +353,24 @@ TEST_F(StringsTest, IncrbyTest) {
   std::string value;
   std::map<DataType, Status> type_status;
   std::map<DataType, int64_t> type_ttl;
+  int32_t expired_timestamp_sec;
 
   // ***************** Group 1 Test *****************
   // If the key is not exist
-  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
 
   // If the key contains a string that can not be represented as integer
   s = db.Set("GP1_INCRBY_KEY", "INCRBY_VALUE");
   ASSERT_TRUE(s.ok());
-  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 5, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.IsCorruption());
 
   s = db.Set("GP1_INCRBY_KEY", "1");
   ASSERT_TRUE(s.ok());
   // Less than the maximum number 9223372036854775807
-  s = db.Incrby("GP1_INCRBY_KEY", 9223372036854775807, &ret);
+  s = db.Incrby("GP1_INCRBY_KEY", 9223372036854775807, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.IsInvalidArgument());
 
   // ***************** Group 2 Test *****************
@@ -380,7 +383,7 @@ TEST_F(StringsTest, IncrbyTest) {
   ASSERT_LE(type_ttl[kStrings], 100);
   ASSERT_GE(type_ttl[kStrings], 0);
 
-  s = db.Incrby("GP2_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP2_INCRBY_KEY", 5, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 15);
   s = db.Get("GP2_INCRBY_KEY", &value);
@@ -395,7 +398,7 @@ TEST_F(StringsTest, IncrbyTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_INCRBY_KEY");
 
-  s = db.Incrby("GP3_INCRBY_KEY", 5, &ret);
+  s = db.Incrby("GP3_INCRBY_KEY", 5, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 5);
   s = db.Get("GP3_INCRBY_KEY", &value);
@@ -409,7 +412,7 @@ TEST_F(StringsTest, IncrbyTest) {
   s = db.Set("GP4_INCRBY_KEY", "50000");
   ASSERT_TRUE(s.ok());
 
-  s = db.Incrby("GP4_INCRBY_KEY", 50000, &ret);
+  s = db.Incrby("GP4_INCRBY_KEY", 50000, &ret, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_EQ(ret, 100000);
   s = db.Get("GP4_INCRBY_KEY", &value);
@@ -422,22 +425,23 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   std::string value;
   std::map<DataType, Status> type_status;
   std::map<DataType, int64_t> type_ttl;
+  int32_t expired_timestamp_sec;
   double eps = 0.1;
 
   // ***************** Group 1 Test *****************
   s = db.Set("GP1_INCRBYFLOAT_KEY", "10.50");
   ASSERT_TRUE(s.ok());
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "0.1", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "0.1", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 10.6, eps);
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "-5", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "-5", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 5.6, eps);
 
   // If the key contains a string that can not be represented as integer
   s = db.Set("GP1_INCRBYFLOAT_KEY", "INCRBY_VALUE");
   ASSERT_TRUE(s.ok());
-  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "5", &value);
+  s = db.Incrbyfloat("GP1_INCRBYFLOAT_KEY", "5", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.IsCorruption());
 
   // ***************** Group 2 Test *****************
@@ -450,7 +454,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   ASSERT_LE(type_ttl[kStrings], 100);
   ASSERT_GE(type_ttl[kStrings], 0);
 
-  s = db.Incrbyfloat("GP2_INCRBYFLOAT_KEY", "10.22222", &value);
+  s = db.Incrbyfloat("GP2_INCRBYFLOAT_KEY", "10.22222", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 20.33333, eps);
   s = db.Get("GP2_INCRBYFLOAT_KEY", &value);
@@ -465,7 +469,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   ASSERT_TRUE(s.ok());
   make_expired(&db, "GP3_INCRBYFLOAT_KEY");
 
-  s = db.Incrbyfloat("GP3_INCRBYFLOAT_KEY", "0.123456", &value);
+  s = db.Incrbyfloat("GP3_INCRBYFLOAT_KEY", "0.123456", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 0.123456, eps);
   s = db.Get("GP3_INCRBYFLOAT_KEY", &value);
@@ -479,7 +483,7 @@ TEST_F(StringsTest, IncrbyfloatTest) {
   s = db.Set("GP4_INCRBYFLOAT_KEY", "100.001");
   ASSERT_TRUE(s.ok());
 
-  s = db.Incrbyfloat("GP4_INCRBYFLOAT_KEY", "11.11", &value);
+  s = db.Incrbyfloat("GP4_INCRBYFLOAT_KEY", "11.11", &value, &expired_timestamp_sec);
   ASSERT_TRUE(s.ok());
   ASSERT_NEAR(std::stod(value), 111.111, eps);
   s = db.Get("GP4_INCRBYFLOAT_KEY", &value);