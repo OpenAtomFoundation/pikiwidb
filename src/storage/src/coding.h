54841d8f04ac40ec3de37f6e79943dd7e4e5ec84
chienguo <2778180460@qq.com>
2024-10-28 10:16:03 +0800

    feat: add support for vector search (ospp 2024) (#2914)
    * add storage support for vector search

--- src/storage/src/coding.h
+++ src/storage/src/coding.h
@@ -29,6 +29,23 @@ namespace storage {
 static const bool kLittleEndian = STORAGE_PLATFORM_IS_LITTLE_ENDIAN;
 #undef STORAGE_PLATFORM_IS_LITTLE_ENDIAN
 
+inline void EncodeFixed8(char* buf, uint8_t value) {
+  if (kLittleEndian) {
+    memcpy(buf, &value, sizeof(value));
+  } else {
+    buf[0] = value & 0xff;
+  }
+}
+
+inline void EncodeFixed16(char* buf, uint16_t value) {
+  if (kLittleEndian) {
+    memcpy(buf, &value, sizeof(value));
+  } else {
+    buf[0] = value & 0xff;
+    buf[1] = (value >> 8) & 0xff;
+  }
+}
+
 inline void EncodeFixed32(char* buf, uint32_t value) {
   if (kLittleEndian) {
     memcpy(buf, &value, sizeof(value));
@@ -52,6 +69,29 @@ inline void EncodeFixed64(char* buf, uint64_t value) {
     buf[5] = (value >> 40) & 0xff;
     buf[6] = (value >> 48) & 0xff;
     buf[7] = (value >> 56) & 0xff;
+  }
+}
+
+inline uint8_t DecodeFixed8(const char* ptr) {
+  if (kLittleEndian) {
+    // Load the raw bytes
+    uint8_t result;
+    memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+    return result;
+  } else {
+    return static_cast<uint8_t>(static_cast<unsigned char>(ptr[0]));
+  }
+}
+
+inline uint16_t DecodeFixed16(const char* ptr) {
+  if (kLittleEndian) {
+    // Load the raw bytes
+    uint16_t result;
+    memcpy(&result, ptr, sizeof(result));  // gcc optimizes this to a plain load
+    return result;
+  } else {
+    return ((static_cast<uint16_t>(static_cast<unsigned char>(ptr[0]))) |
+            (static_cast<uint16_t>(static_cast<unsigned char>(ptr[1])) << 8));
   }
 }
 
@@ -81,6 +121,7 @@ inline uint64_t DecodeFixed64(const char* ptr) {
     return (hi << 32) | lo;
   }
 }
+
 
 }  // namespace storage
 #endif  // SRC_CODING_H_
