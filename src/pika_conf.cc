4b5a58ee7856a9bffad68aa0036781684e6b15d7
cheniujh <1271435567@qq.com>
2024-12-05 15:51:40 +0800

    auto reset log-level to 0 if pika.conf has given an invalid value

--- src/pika_conf.cc
+++ src/pika_conf.cc
@@ -135,6 +135,10 @@ int PikaConf::Load() {
 
   int32_t log_level = 0;
   GetConfInt("log-level", &log_level);
+  if (log_level != 0 && log_level != 1) {
+    LOG(ERROR) << "log-level loaded from pika.conf is invalid, auto change it to 0";
+    log_level = 0;
+  }
   log_level_.store(log_level);
 
   GetConfStr("db-path", &db_path_);
