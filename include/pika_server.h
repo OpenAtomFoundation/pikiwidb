556bc436fd2288166ff6dcdc04d974da3e09831c
Mixficsol <838844609@qq.com>
2025-03-20 11:08:13 +0800

    Revert "fix: fix exit core (#3050)" (#3052)
    This reverts commit 5c2ecdb6d88e9c2b9ba6d1f285a52ca4b29cebdd.

--- include/pika_server.h
+++ include/pika_server.h
@@ -259,7 +259,6 @@ class PikaServer : public pstd::noncopyable {
   void ResetStat();
   void incr_accumulative_connections();
   void ResetLastSecQuerynum();
-  void ResetCommandCount();
   void UpdateQueryNumAndExecCountDB(const std::string& db_name, const std::string& command, bool is_write);
   std::unordered_map<std::string, uint64_t> ServerExecCountDB();
   std::unordered_map<std::string, QpsStatistic> ServerAllDBStat();
