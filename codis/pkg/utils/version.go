d26dd3af0b3d4f099a13c8bf58f6d944221d7b39
Mixficsol <838844609@qq.com>
2025-03-21 18:55:17 +0800

    fix: Fixed versions of Pika, Pika_exporter, Codis (#3057)
    * Fixed versions of Pika, Pika_exporter, Codis
    
    * fix Makefile
    
    * fill code
    
    ---------
    
    Co-authored-by: wuxianrong <wuxianrong@360.cn>

--- codis/pkg/utils/version.go
+++ codis/pkg/utils/version.go
@@ -1,6 +1,8 @@
 package utils
 
 const (
-	Version = "2018-11-04 16:22:35 +0800 @de1ad026e329561c22e2a3035fbfe89dc7fef764 @3.2.2-12-gde1ad026"
-	Compile = "2023-02-23 11:25:09 +0800 by go version go1.19.6 linux/amd64"
+	Version   = "3.5.4"
+	Gitsha    = "61a9b5a6737759459a50684b8c018338ebe5808e"
+	Compile   = "2025-03-21 17:26:57 CST"
+	GoVersion = "go1.23.4 darwin/amd64"
 )
