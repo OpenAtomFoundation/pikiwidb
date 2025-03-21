bce76c2585fa94b217236d497f71fe5b3217bbbb
wuxianrong <wuxianrong@360.cn>
2025-03-21 18:53:28 +0800

    fill code

--- tools/pika_exporter/version.go
+++ tools/pika_exporter/version.go
@@ -1,9 +1,8 @@
 package main
 
 const (
-	PikaExporterVersion = "3.5.4"
-	BuildVersion        = "fix_codis"
-	BuildCommitSha      = "61a9b5a6737759459a50684b8c018338ebe5808e"
-	BuildDate           = "2025-03-21 17:15:41 CST"
-	GoVersion           = "go1.23.4 darwin/amd64"
+	BuildVersion   = "Filled in by build"
+	BuildCommitSha = "Filled in by build"
+	BuildDate      = "Filled in by build"
+	GoVersion      = "Filled in by build"
 )
