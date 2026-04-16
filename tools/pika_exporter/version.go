95918157da1779ced5539d8b14dbd1b51765418b
chejinge <chejinge@360.cn>
2026-04-16 20:22:11 +0800

    fix:complie

--- tools/pika_exporter/version.go
+++ tools/pika_exporter/version.go
@@ -2,8 +2,8 @@ package main
 
 const (
 	PikaExporterVersion = "3.5.5"
-	BuildVersion        = "Filled in by build"
-	BuildCommitSha      = "Filled in by build"
-	BuildDate           = "Filled in by build"
-	GoVersion           = "Filled in by build"
+	BuildVersion        = "3.5"
+	BuildCommitSha      = "5ed38e4f26e589fc0c5e8cb1c938cc5d6add9e62"
+	BuildDate           = "2026-04-16 20:08:51 CST"
+	GoVersion           = "go1.22.1 darwin/amd64"
 )
